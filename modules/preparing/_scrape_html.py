8221a692d9d5650e163e40635117ca447b813037
Hiroki Chiyoda <dijzpeb2@gmail.com>
2022-06-23 22:11:18 +0900

    htmlファイルが存在する場合もスクレイピングを実行するコードになっていたので、修正。

--- modules/preparing/_scrape_html.py
+++ modules/preparing/_scrape_html.py
@@ -12,16 +12,16 @@ def scrape_html_race(race_id_list: list, skip: bool = True):
     """
     html_path_list = []
     for race_id in tqdm(race_id_list):
-        url = UrlPaths.RACE_URL + race_id #race_idからurlを作る
-        time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
-        html = urlopen(url).read() #スクレイピング実行
-        filename = os.path.join(LocalDirs.HTML_RACE_DIR, race_id+'.bin')
-        html_path_list.append(filename)
+        filename = os.path.join(LocalDirs.HTML_RACE_DIR, race_id+'.bin') #保存するファイル名
         if skip and os.path.isfile(filename): #skipがTrueで、かつbinファイルがすでに存在する場合は飛ばす
             print('race_id {} skipped'.format(race_id))
-            continue
-        with open(filename, 'wb') as f: #保存するファイルパスを指定
-            f.write(html) #保存
+        else:
+            url = UrlPaths.RACE_URL + race_id #race_idからurlを作る
+            html = urlopen(url).read() #スクレイピング実行
+            time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
+            with open(filename, 'wb') as f: #保存するファイルパスを指定
+                f.write(html) #保存
+        html_path_list.append(filename)
     return html_path_list
 
 def scrape_html_horse(horse_id_list: list, skip: bool = True):
@@ -31,16 +31,16 @@ def scrape_html_horse(horse_id_list: list, skip: bool = True):
     """
     html_path_list = []
     for horse_id in tqdm(horse_id_list):
-        url = UrlPaths.HORSE_URL + horse_id #horse_idからurlを作る
-        time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
-        html = urlopen(url).read() #スクレイピング実行
-        filename = os.path.join(LocalDirs.HTML_HORSE_DIR, horse_id+'.bin')
-        html_path_list.append(filename)
+        filename = os.path.join(LocalDirs.HTML_HORSE_DIR, horse_id+'.bin') #保存するファイル名
         if skip and os.path.isfile(filename): #skipがTrueで、かつbinファイルがすでに存在する場合は飛ばす
             print('horse_id {} skipped'.format(horse_id))
-            continue
-        with open(filename, 'wb') as f: #保存するファイルパスを指定
-            f.write(html) #保存
+        else:
+            url = UrlPaths.HORSE_URL + horse_id #horse_idからurlを作る
+            time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
+            html = urlopen(url).read() #スクレイピング実行
+            with open(filename, 'wb') as f: #保存するファイルパスを指定
+                f.write(html) #保存
+        html_path_list.append(filename)
     return html_path_list
 
 def scrape_html_ped(horse_id_list: list, skip: bool = True):
@@ -50,16 +50,17 @@ def scrape_html_ped(horse_id_list: list, skip: bool = True):
     """
     html_path_list = []
     for horse_id in tqdm(horse_id_list):
-        url = UrlPaths.PED_URL + horse_id #horse_idからurlを作る
-        time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
-        html = urlopen(url).read() #スクレイピング実行
-        filename = os.path.join(LocalDirs.HTML_PED_DIR, horse_id+'.bin')
-        html_path_list.append(filename)
+        filename = os.path.join(LocalDirs.HTML_PED_DIR, horse_id+'.bin') #保存するファイル名
         if skip and os.path.isfile(filename): #skipがTrueで、かつbinファイルがすでに存在する場合は飛ばす
             print('horse_id {} skipped'.format(horse_id))
             continue
-        with open(filename, 'wb') as f: #保存するファイルパスを指定
-            f.write(html) #保存
+        else:
+            url = UrlPaths.PED_URL + horse_id #horse_idからurlを作る
+            time.sleep(1) #相手サーバーに負担をかけないように1秒待機する
+            html = urlopen(url).read() #スクレイピング実行
+            with open(filename, 'wb') as f: #保存するファイルパスを指定
+                f.write(html) #保存
+        html_path_list.append(filename)
     return html_path_list
 
 #def scrape_html_horse_with_hogehoge():
