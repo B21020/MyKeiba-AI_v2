445f094402d7c69e1ca1a8333b2a489f9a09ca33
koike <yoship2006@yahoo.co.jp>
2022-07-12 05:21:38 +0900

    タイムを秒変換する処理とコメント修正

--- modules/preprocessing/_horse_results_processor.py
+++ modules/preprocessing/_horse_results_processor.py
@@ -13,23 +13,30 @@ class HorseResultsProcessor(AbstractDataProcessor):
     def _preprocess(self):
         df = self.raw_data
 
-        # 着順に数字以外の文字列が含まれているものを取り除く
+        # 不要な行を削除する
+        # タイムが「x:xx.x」以外の行
+        df = df.dropna(subset=[Cols.TIME])
+        df = df[df[Cols.TIME].str.contains('\d{1}:\d{2}\.\d{1}')]
+        
+        # 着順に数字以外の文字列が含まれているものは、欠損値（NaN）に置き換える
         # サイト上のテーブルに存在する列名は、HorseResultsColsクラスで定数化している。
         df[Cols.RANK] = pd.to_numeric(df[Cols.RANK], errors='coerce')
+        # 着順が欠損値（NaN）となったものを取り除く
         df.dropna(subset=[Cols.RANK], inplace=True)
         df[Cols.RANK] = df[Cols.RANK].astype(int)
 
+        # 日付をdatetime型に設定
         df["date"] = pd.to_datetime(df[Cols.DATE])
         df.drop([Cols.DATE], axis=1, inplace=True)
         
-        #賞金のNaNを0で埋める
+        # 賞金のNaNを0で埋める
         df[Cols.PRIZE].fillna(0, inplace=True)
         
-        #1着の着差を0にする
+        # 1着の着差を0にする（xが0より小さい場合は、0、xが0以上の場合、xを返す）
         df[Cols.RANK_DIFF] = df[Cols.RANK_DIFF].map(lambda x: 0 if x<0 else x)
         
-        #レース展開データ
-        #n=1: 最初のコーナー位置, n=4: 最終コーナー位置
+        # レース展開データ
+        # n=1: 最初のコーナー位置, n=4: 最終コーナー位置
         def corner(x, n):
             if type(x) != str:
                 return x
@@ -37,6 +44,7 @@ class HorseResultsProcessor(AbstractDataProcessor):
                 return int(re.findall(r'\d+', x)[-1])
             elif n==1:
                 return int(re.findall(r'\d+', x)[0])
+
         df['first_corner'] = df[Cols.CORNER].map(lambda x: corner(x, 1))
         df['final_corner'] = df[Cols.CORNER].map(lambda x: corner(x, 4))
         
@@ -49,10 +57,19 @@ class HorseResultsProcessor(AbstractDataProcessor):
         
         # race_type（数字以外の文字列を抽出）
         df['race_type'] = df[Cols.RACE_TYPE_COURSE_LEN].str.extract(r'(\D+)')[0].map(Master.RACE_TYPE_DICT)
-        #距離は10の位を切り捨てる
+        # 距離は10の位を切り捨てる
         df['course_len'] = df[Cols.RACE_TYPE_COURSE_LEN].str.extract(r'(\d+)').astype(int) // 100
+        # 距離列を削除
         df.drop([Cols.RACE_TYPE_COURSE_LEN], axis=1, inplace=True)
-        #インデックス名を与える
+        
+        # タイムの値を秒単位に変換
+        base_time = pd.to_datetime("00:00.0", format="%M:%S.%f")
+        df['time_seconds'] = pd.to_datetime(df[Cols.TIME], format="%M:%S.%f") - base_time
+        df['time_seconds'] = df['time_seconds'].dt.total_seconds()
+        # タイム列を削除
+        df.drop([Cols.TIME], axis=1, inplace=True)
+
+        # インデックス名を与える
         df.index.name = 'horse_id'
         
         return df
