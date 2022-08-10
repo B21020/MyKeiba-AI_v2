19b1aeb455b12af46045afd45605ae288d9df1ba
Hiroki Chiyoda <dijzpeb2@gmail.com>
2022-08-10 09:48:24 +0900

    コメントを修正・追加

--- modules/policies/_score_policy.py
+++ modules/policies/_score_policy.py
@@ -22,27 +22,30 @@ class BasicScorePolicy(AbstractScorePolicy):
 
 class StdScorePolicy(AbstractScorePolicy):
     """
-    レース内で標準化して、相対評価する。「レース内偏差値」みたいなもの。
+    レース内で標準化して、相対評価する。「レース内偏差値」のようなもの。
     """
     @staticmethod
     def calc(model, X: pd.DataFrame):
         score_table = X[[ResultsCols.UMABAN, ResultsCols.TANSHO_ODDS]].copy()
         score = model.predict_proba(X.drop([ResultsCols.TANSHO_ODDS], axis=1))[:, 1]
         score_table['score'] = score
+        # レース内でスコアを標準化
         standard_scaler = lambda x: (x - x.mean()) / x.std(ddof=0)
         score_table['score'] = score_table['score'].groupby(level=0).transform(standard_scaler)
         return score_table
     
 class MinMaxScorePolicy(AbstractScorePolicy):
     """
-    レース内で標準化して、相対評価すした後、全体を0~1にスケーリング。
+    レース内で標準化して、相対評価した後、全体を0~1にスケーリング。
     """
     @staticmethod
     def calc(model, X: pd.DataFrame):
         score_table = X[[ResultsCols.UMABAN, ResultsCols.TANSHO_ODDS]].copy()
         score = model.predict_proba(X.drop([ResultsCols.TANSHO_ODDS], axis=1))[:, 1]
+        # レース内でスコアを標準化
         standard_scaler = lambda x: (x - x.mean()) / x.std(ddof=0)
         score = score.groupby(level=0).transform(standard_scaler)
+        # データ全体で0~1にスケーリング
         score = (score - score.min()) / (score.max() - score.min())
         score_table['score'] = score
         return score_table
