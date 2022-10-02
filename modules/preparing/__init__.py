19c0caaa32befdf482e8e117bd0075ffb837fb62
koike <yoship2006@yahoo.co.jp>
2022-10-02 20:42:11 +0900

    レビュー指摘を反映

--- modules/preparing/__init__.py
+++ modules/preparing/__init__.py
@@ -1,4 +1,5 @@
-from ._scrape_race_id_list import scrape_kaisai_date, scrape_race_id_list, scrape_race_id_race_time_list, create_active_race_id_list
+from ._scrape_race_id_list import scrape_kaisai_date, scrape_race_id_list
+from ._create_active_race_id_list import scrape_race_id_race_time_list, create_active_race_id_list
 from ._scrape_html import scrape_html_horse, scrape_html_ped, scrape_html_race,\
     scrape_html_horse_with_master
 from ._get_rawdata import get_rawdata_horse_results, get_rawdata_horse_info, get_rawdata_info, get_rawdata_peds,\
